c83fedac38fb3eda39c26efcda5be0c27d6a5564
sor <s6301012630176@email.kmutnb.ac.th>
2022-02-21 15:48:51 +0700

    Change Static Board to Dynamic Board

--- src/board.js
+++ src/board.js
@@ -12,25 +12,28 @@ class Board extends React.Component {
             />
         );
     }
+    
+    createTable = () => {
+        let table = []
+        let n = 0
+        for (let i = 0; i < 4; i++) {
+            let row = []
+            for (let j = 0; j < 4; j++) {
+                row.push(
+                    this.renderSquare(n)
+                )
+                n++
+            }
+            n++
+            table.push(<div className="board-row">{row}</div>)
+        }
+        return table
+    }
 
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {this.createTable()}
             </div>
         );
     }
